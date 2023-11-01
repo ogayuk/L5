fb475e78a04a284011fd17029486941039667968
ogawayuki <2111140072d@ed.fuk.kindai.ac.jp>
2023-11-01 06:52:49 +0000

    L6_01

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  #def create
+    #p = BCrypt::Password.create(params[:user][:pass])
+    #user = User.new(uid: params[:user][:uid], pass: p, age: params[:user][:age])
+    #user.save
+    #redirect_to users_path
+  #end
+  
   def create
-    p = BCrypt::Password.create(params[:user][:pass])
-    user = User.new(uid: params[:user][:uid], pass: p, age: params[:user][:age])
-    user.save
-    redirect_to users_path
+    @user = User.new(
+      uid: params[:user][:uid],
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation])
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
   end
+  
   
   def destroy
     User.find(params[:id]).destroy
